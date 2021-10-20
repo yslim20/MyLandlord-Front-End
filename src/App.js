b2ba7f72a93c5af5f389501fbad21688a91d6143
caliLai <calilai2018@gmail.com>
2021-10-20 11:28:17 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+// import axios from 'axios';
+
+function App() {
+	function httpGet(url)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+	// const m = axios.get("/hi").then(a => console.log(a))
+	// .catch(e => console.log(e))
+  return (
+    <div className="App">
+      <header>
+        <h1>{httpGet("/hi")}</h1>
+        <h1>{httpGet("/hey")}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
