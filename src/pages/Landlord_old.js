45e0ec482e0c332fe8e26187100de3cd2fcbcb35
yslim20 <ylim27@my.bcit.ca>
2021-11-08 18:34:26 -0800

    finished searchbar comp

--- src/pages/Landlord_old.js
+++ src/pages/Landlord_old.js
@@ -23,54 +23,46 @@ const MidCont = styled.div`
 	display:flex;
 	flex: 1;
 	flex-direction: column;
-	justify-content:center;
-	align-items:flex-start;
+	justify-content:right;
+	align-items:right;
 	margin-bottom: 100px;
-  padding: 0 68px 0 68px;
+    width:80%;
 `
 //the filter bar with 2 icon 
 const TopBar = styled.div`
-  display:flex;
-  flex:1;
-  flex-firection:row;
-  justify-content:right;
+    display:flex;
+    flex:1;
+    flex-firection:row;
+    justify-content:right;
 	align-items:right;
-`
+    `
 
 //building the page, 
 export default function Landlord() {
 	return(
 		<Cont>
 			<TopNav/>
-      <MidCont>
-        <Header text="Map"
-        />
-        <SubHead 
-          align="left" 
-          marginL="100" 
-          justifyContent="left"
-          text="Which area do you want to search"
-        />
-        <SearchBar/>
-        <ImgBox cwidth="50%" cheight="600"/>
+            <MidCont>
+                <Header text="Map"
+                />
+                <SubHead align="left" marginL="100" justifyContent="left"
+                    text="Which area do you want to search"/>
+                    <SearchBar/>
+                    <ImgBox cwidth="50%" cheight="600"/>
 
-      </MidCont>
-      <MidCont>
-        <Header text="List of Landlord" 
-        />
-        <SubHead 
-          align="left" 
-          marginL="100" 
-          justifyContent="left"            
-          text="Who do you want to search"
-        />
-        <SearchBar/>
-        
-        <MuiTable/>
-          
-      </MidCont>
+            </MidCont>
+            <MidCont>
+                <Header text="List of Landlord" 
+                />
+                <SubHead align="left" marginL="100" justifyContent="left"
+                    text="Who do you want to search"/>
+                <SearchBar/>
+                
+                <MuiTable/>
+                
+            </MidCont>
 
-    </Cont>   
+         </Cont>   
 
     )
 }
