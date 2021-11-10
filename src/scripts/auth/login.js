a70d4f0b4c115146a9dc3b06ffc22efccf11523e
caliLai <calilai2018@gmail.com>
2021-11-10 10:29:11 -0800

    irrelevent message

--- src/scripts/auth/login.js
+++ src/scripts/auth/login.js
@@ -1,19 +1,21 @@
-const login = async event => {
+const login = async (event) => {
 	event.preventDefault();
 
+	console.log(event.target);
+
 	// idk where this going dude
-	const response = await fetch('/auth/login', {
-		body: JSON.stringify({
-        	email: event.target.email.value,
-        	password: event.target.password.value,
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-  });
-  const successfail = await response.json();
-  console.log(successfail);
+	// const response = await fetch('/auth/login', {
+	// 	body: JSON.stringify({
+  //       	email: event.target.email.value,
+  //       	password: event.target.Password.value,
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     method: 'POST'
+  // });
+  // const successfail = await response.json();
+  // console.log(successfail);
 }
 
 export default login;
