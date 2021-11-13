f076acedc4ee16961d2184a386b7d3cd6d16136b
yslim20 <ylim27@my.bcit.ca>
2021-11-12 17:22:03 -0800

    removed event inside of login() function

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -56,7 +56,7 @@ export default function LogIn() {
             <TopNav />
 {/* // ============ Content start*/}
             <Header />
-            <BttmCont onSubmit={()=>login(event)} >
+            <BttmCont onSubmit={()=>login()} >
 {/* // ============ Form*/}
                 <ContLeft>
                     <Input title="Email" type ="email"/>
